dc6e7eda57b7c20fcf6a751d82b0d766130171d6
Jose Mesa <jolemeag@gmail.com>
2022-10-18 13:01:32 -0500

    version 1.0.0

---
+++ src/models/pedido.model.ts
@@ -0,0 +1,64 @@
+import {Entity, model, property, belongsTo, hasOne} from '@loopback/repository';
+import {Persona} from './persona.model';
+import {Producto} from './producto.model';
+
+@model({settings: {strict: false}})
+export class Pedido extends Entity {
+  @property({
+    type: 'string',
+    id: true,
+    generated: true,
+  })
+  Id?: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  IdProducto: string;
+
+  @property({
+    type: 'number',
+    required: true,
+  })
+  Cantidad: number;
+
+  @property({
+    type: 'number',
+    required: true,
+  })
+  Total: number;
+
+  @property({
+    type: 'number',
+    required: true,
+  })
+  Estado: number;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  IdPersona: string;
+
+  @belongsTo(() => Persona)
+  personaId: string;
+
+  @hasOne(() => Producto)
+  producto: Producto;
+  // Define well-known properties here
+
+  // Indexer property to allow additional data
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  [prop: string]: any;
+
+  constructor(data?: Partial<Pedido>) {
+    super(data);
+  }
+}
+
+export interface PedidoRelations {
+  // describe navigational properties here
+}
+
+export type PedidoWithRelations = Pedido & PedidoRelations;
---
+++ src/models/persona.model.ts
@@ -0,0 +1,60 @@
+import {Entity, model, property, hasMany} from '@loopback/repository';
+import {Pedido} from './pedido.model';
+
+@model({settings: {strict: false}})
+export class Persona extends Entity {
+  @property({
+    type: 'string',
+    id: true,
+    generated: true,
+  })
+  Id?: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  Nombres: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  Apellidos: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  Correo: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  Celular: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  Clave: string;
+
+  @hasMany(() => Pedido)
+  pedidos: Pedido[];
+  // Define well-known properties here
+
+  // Indexer property to allow additional data
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  [prop: string]: any;
+
+  constructor(data?: Partial<Persona>) {
+    super(data);
+  }
+}
+
+export interface PersonaRelations {
+  // describe navigational properties here
+}
+
+export type PersonaWithRelations = Persona & PersonaRelations;
